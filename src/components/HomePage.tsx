ac05a33e21d9bda07e0cb1bc0b71644fbcdf1bd9
Marcin Czepiela <czepimm200@gmail.com>
2020-08-17 16:51:22 +0200

    Added sass

---
+++ src/components/HomePage.tsx
@@ -0,0 +1,7 @@
+import React, { FunctionComponent } from "react";
+
+type HomePageProps = {};
+
+export const HomaPage: FunctionComponent<HomePageProps> = (props: any) => {
+  return <button>Guzik</button>;
+};
---
+++ src/components/MainPage.tsx
@@ -0,0 +1,7 @@
+import React, { FunctionComponent } from "react";
+
+type MainPageProps = {};
+
+export const HomaPage: FunctionComponent<MainPageProps> = (props: any) => {
+  return <button>Guzik</button>;
+};
